c28f163b8b4b2649ab3546a3c186680de921f309
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-05-22 11:57:00 -0400

    "sofar"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Cohart from "./Cohart";
 import Students from "./Students";
 // how do you import the data.json for the cohart useState???
 
@@ -14,6 +15,7 @@ function App() {
     <div>
       <h1>Student Dashboard</h1>
       <Students></Students>
+      <Cohart></Cohart>
     </div>
   );
 }
---
+++ src/Cohart.js
@@ -0,0 +1,15 @@
+  import React from "react";
+
+  const Cohart = () => {
+
+
+    return(
+        <div>
+            <h1>Choose A Class by Start Date</h1>
+
+        </div>
+    )
+  }
+
+
+  export default Cohart;
