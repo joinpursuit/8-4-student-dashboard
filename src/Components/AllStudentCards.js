9758eef7533a01b246a0e7b9d4b630afcb0b4332
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-05-23 22:18:43 -0400

    I'm going to get there

---
+++ src/Components/AllStudentCards.js
@@ -0,0 +1,13 @@
+import StudentCard from "./StudentCard";
+const AllStudentCards = (props) => {
+  return (
+    <div>
+        <h2>All Students</h2>
+        <p>Total Students: {props.students.length}</p>
+      {props.students.map((student) => {
+        return <StudentCard student={student}> </StudentCard>;
+      })}
+    </div>
+  );
+};
+export default AllStudentCards;
