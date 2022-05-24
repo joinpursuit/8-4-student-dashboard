2607552e531a858c4280e06950989151242889ee
T.K <nique@niques-air.myfiosgateway.com>
2022-05-23 21:21:31 -0400

    try

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { useState } from "react";
+import CardContainer from "./CardContainer";
+import CohortList from "./CohortList";
+import data from "./data/data.json"
+import "./index.css";
 
-function App() {
+const App = () => {
+
+  const [cohort, setCohort] = useState(data);
+  const [heading, setHeading] = useState("All Students")
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
+    <div className="grid">
+      <h1 className="header">Student Dashboard</h1>
+    <CohortList cohort={cohort} setcohort={setCohort} setHeading={setHeading} data={data}/>
+    <CardContainer data={data} setCohort={setCohort} heading={heading}/>
     </div>
   );
 }
